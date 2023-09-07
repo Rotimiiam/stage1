d690867f8be4c71b65b78395dab2224b4710ff15
Rotimiiam <rotimiiam@gmail.com>
2023-09-07 14:22:44 +0100

    fix

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 function get_current_utc_time() {
     $current_time = new DateTime('now', new DateTimeZone('UTC'));
     $target_time = new DateTime('now', new DateTimeZone('UTC'));
-    $target_time->modify('-1 minutes');
+    $target_time->modify('-2 minutes');
     
     if ($current_time <= $target_time) {
         return $current_time->format('Y-m-d\TH:i:s\Z');
@@ -20,8 +20,8 @@ $track = isset($_GET['track']) ? $_GET['track'] : null;
 if ($slack_name && $track) {
     $current_day = date('l');
     $utc_time = get_current_utc_time();
-    $github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"; // Replace with your GitHub file URL
-    $github_repo_url = "https://github.com/username/repo"; // Replace with your GitHub repo URL
+    $github_file_url = "https://github.com/Rotimiiam/stage1/blob/main/api.php"; // Replace with your GitHub file URL
+    $github_repo_url = "https://github.com/Rotimiiam/stage1"; // Replace with your GitHub repo URL
 
     $response_data = array(
         "slack_name" => $slack_name,
