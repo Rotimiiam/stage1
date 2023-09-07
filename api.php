661d01827267f9f2cc54965438dd4f60fb33d69f
Rotimiiam <rotimiiam@gmail.com>
2023-09-07 14:14:43 +0100

    time

--- api.php
+++ api.php
@@ -1,38 +1,41 @@
 <?php
-// Get the slack_name and track parameters from the GET request
-$slack_name = $_GET["slack_name"];
-$track = $_GET["track"];
+header('Content-Type: application/json');
 
-// Validate the parameters
-if (empty($slack_name) || empty($track)) {
-  // Return an error message and status code 400 if any parameter is missing
-  echo json_encode(array(
-    "error_message" => "Please provide both slack name and track parameters",
-    "status_code" => 400
-  ));
-  exit();
+function get_current_utc_time() {
+    $current_time = new DateTime('now', new DateTimeZone('UTC'));
+    $target_time = new DateTime('now', new DateTimeZone('UTC'));
+    $target_time->modify('-2 minutes');
+    
+    if ($current_time <= $target_time) {
+        return $current_time->format('Y-m-d\TH:i:s\Z');
+    } else {
+        http_response_code(400);
+        return json_encode(array("error" => "Validation Error: Current time not within +/-2 minutes of UTC."));
+    }
 }
 
-// Get the current day of the week
-$current_day = date("l");
+$slack_name = isset($_GET['slack_name']) ? $_GET['slack_name'] : null;
+$track = isset($_GET['track']) ? $_GET['track'] : null;
 
-// Get the current UTC time
-$utc_time = gmdate("Y-m-d\TH:i:s\Z");
+if ($slack_name && $track) {
+    $current_day = date('l');
+    $utc_time = get_current_utc_time();
+    $github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"; // Replace with your GitHub file URL
+    $github_repo_url = "https://github.com/username/repo"; // Replace with your GitHub repo URL
 
-// Get the GitHub file URL of the current script
-$github_file_url = "https://github.com/Rotimiiam/stage1/blob/main/api.php";
+    $response_data = array(
+        "slack_name" => $slack_name,
+        "current_day" => $current_day,
+        "utc_time" => $utc_time,
+        "track" => $track,
+        "github_file_url" => $github_file_url,
+        "github_repo_url" => $github_repo_url,
+        "status_code" => 200
+    );
 
-// Get the GitHub repo URL of the project
-$github_repo_url = "https://github.com/Rotimiiam/stage1";
-
-// Return the JSON response with the required information and status code 200
-echo json_encode(array(
-  "slack_name" => $slack_name,
-  "current_day" => $current_day,
-  "utc_time" => $utc_time,
-  "track" => $track,
-  "github_file_url" => $github_file_url,
-  "github_repo_url" => $github_repo_url,
-  "status_code" => 200
-));
+    echo json_encode($response_data);
+} else {
+    http_response_code(400);
+    echo json_encode(array("error" => "Missing parameters"));
+}
 ?>
